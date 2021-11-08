bac7cc9d4855fa1337f8569a486889294fe17c09
Vedant Deokar <vedant1903@gmail.com>
2021-11-08 03:49:39 -0500

    start apolloServer asynchronously
    Apollo server 2 approach of listening or starting the server doesnt work anymore. The server needs to be started asynchronously

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const http = require("http");
+require("dotenv").config();
+
+const typeDefs = require("./src/graphql/typeDefs");
+const resolvers = require("./src/graphql/resolvers");
+const { buildServerContext } = require("./src/graphql/graphql");
+
+const port = process.env.SERVER_PORT || 8000;
 const app = express();
-const port = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const httpServer = http.createServer(app);
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: (req) => buildServerContext(req),
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  await new Promise((resolve) => httpServer.listen({ port }, resolve));
+  console.log(
+    `Server started at http://localhost:${port}${apolloServer.graphqlPath}`
+  );
+}
+
+startApolloServer(typeDefs, resolvers);
